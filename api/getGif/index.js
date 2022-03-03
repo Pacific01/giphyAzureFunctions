1d1ba30a58ccc7c1b6e8733390619a8808640fdd
Jaime Pascual Torres <jpascual@everlyrusher.com>
2022-03-03 15:40:02 +0100

    Add require axios

--- api/getGif/index.js
+++ api/getGif/index.js
@@ -1,6 +1,9 @@
+const axios = require('axios')
 module.exports = async function (context, req) {
+  const name = (req.query.name || (req.body && req.body.name));
+
   try {
-    const response =  await axios.get('http://api.giphy.com/v1/gifs/search?api_key=x4xjAYV288NfnVoOtZCgQbMgjJVxJxGu&q=azure')
+    const response =  await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=x4xjAYV288NfnVoOtZCgQbMgjJVxJxGu&q=${name}`)
     context.log(`statusCode: ${response.statusCode}`);
     context.log(response);
     return response; // or return a custom object using properties from response
